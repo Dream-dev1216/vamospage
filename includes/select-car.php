065163a45a0637f491fcaeff8475e8bb1b838b59
Muhammad Talha Zubair <talhazubair1598@gmail.com>
2021-01-23 13:05:43 +0500

    Modals Clickable

--- includes/select-car.php
+++ includes/select-car.php
@@ -27,6 +27,8 @@
       padding: 5px 10px 5px 10px;
       color: white;
       font-weight: bold;
+      border: none;
+      border-radius: 3px;
    }
 
    .cardSubtitlePadding {
@@ -84,6 +86,8 @@
          color: white;
          font-weight: bold;
          margin-bottom: -10px;
+         border: none;
+         border-radius: 3px;
       }
 
       .myRow {
@@ -93,17 +97,18 @@
       }
 
    }
-
-
-
-   /* @media screen and (max-width: 600px) {
-      .cardPriceFormat {
-         font-size: 12px;
-         color: red;
-      }
-   } */
 </style>
-
+<script>
+   var page = 'select';
+   $(document).ready(function() {
+      $(".zoom").click(function() {
+         $("#myModal").modal('show');
+      });
+      $(".secondModal").click(function() {
+         $("#myModal2").modal('show');
+      });
+   });
+</script>
 <!-- Step 2-->
 <div class="container-fluid pb-4 container-grey">
    <?php include 'includes/step-bar.php' ?>
@@ -122,6 +127,78 @@
    </script>
    <div class="pt-scroll2"></div>
    <div class="container p-4 pb-0 shadow-sm mediaBgW">
+
+
+
+
+
+
+
+
+
+      <div class="modal" id="myModal">
+         <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h1 class="modal-title">Premium SUV PFFAR</h1>
+                  <button type="button" class="close" data-dismiss="modal">×</button>
+               </div>
+               <div class="modal-body">
+                  <h4 class="cardPrice">Mitsubishi Montero/ToyotaPrado4x4</h4>
+                  <p>In our fleet a premium SUV is a high-quality, extra-large sport utility vehicle made to seat 7 or 8 passengers in three rows of seating. They also have powerhouse engines and room for plenty of cargo</p>
+                  <p class="carAlign"> <img src="images/cdxr-suzuki-swift-2018-a.png" class="secondModal" alt="Vamos Rent-A-Car"></p>
+                  <p style="font-size:medium;" class="cardPrice">Automatic</p>
+                  <p>Drivers can book from a variety of selections including a 7 passenger Chevrolet Suburban, or a mid-size SUV like the Nissan Rogue, or a passenger van like the Ford Transit. Our SUV and Van rentals are the perfect options to take you the long distance with plenty of space.</p>
+               </div>
+               <div class="modal-footer">
+                  <button type="button" class="cardTitle" data-dismiss="modal">Close</button>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="modal" id="myModal2">
+         <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h1 class="modal-title">Premium SUV PFFAR</h1>
+                  <button type="button" class="close" data-dismiss="modal">×</button>
+               </div>
+               <div class="modal-body">
+                  <p class="carAlign"> <img src="images/cdxr-suzuki-swift-2018-a.png" class="secondModal" alt="Vamos Rent-A-Car"></p>
+               </div>
+               <div class="modal-footer">
+                  <button type="button" class="cardTitle" data-dismiss="modal">Close</button>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <div class="row">
          <div class="rayita d-block d-lg-none"></div>
          <div class="col-12 services">
